3fa3a6b66940ac8f5799b1461ec90e07b497b85f
bala-amypo <bala@amypo.in>
2025-12-18 09:12:30 +0000

    User commit

--- src/main/java/com/example/demo/repository/UserRepo.java
+++ src/main/java/com/example/demo/repository/UserRepo.java
@@ -1,7 +1,7 @@
-package com.example.app.repository;
+package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.app.entity.User;
+import com.example.demo.entity.User;
 
 public interface UserRepo extends JpaRepository<User, Long> {
     User findByEmail(String email);
