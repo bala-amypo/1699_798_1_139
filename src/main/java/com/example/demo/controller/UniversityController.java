d9c13888244d7fe7ea0597c10b67796b7a58dfce
bala-amypo <bala@amypo.in>
2025-12-18 15:07:26 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/UniversityController.java
@@ -0,0 +1,42 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.University;
+import com.example.demo.service.UniversityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/universities")
+@Tag(name = "University API")
+public class UniversityController {
+
+    @Autowired
+    private UniversityService service;
+
+    @PostMapping
+    public University create(@RequestBody University university) {
+        return service.createUniversity(university);
+    }
+
+    @PutMapping("/{id}")
+    public University update(@PathVariable Long id, @RequestBody University university) {
+        return service.updateUniversity(id, university);
+    }
+
+    @GetMapping("/{id}")
+    public University getById(@PathVariable Long id) {
+        return service.getUniversityById(id);
+    }
+
+    @GetMapping
+    public List<University> getAll() {
+        return service.getAllUniversities();
+    }
+
+    @PutMapping("/{id}/deactivate")
+    public void deactivate(@PathVariable Long id) {
+        service.deactivateUniversity(id);
+    }
+}
