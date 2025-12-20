30803f1feb63c1f5376b8abd8ac39a42b1c310dd
bala-amypo <bala@amypo.in>
2025-12-20 06:32:22 +0000

    User commit

--- src/main/java/com/example/demo/entity/TransferRule.java
+++ src/main/java/com/example/demo/entity/TransferRule.java
@@ -1,6 +1,8 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Max;
 
 @Entity
 @Table(name = "transfer_rules")
