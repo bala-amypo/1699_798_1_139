a107384122a65a332fc95598fd3fea1c4b8a5155
bala-amypo <bala@amypo.in>
2025-12-22 16:55:06 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UniversityServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UniversityServiceImpl.java
@@ -1,7 +1,7 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.entity.University;
-import com.example.demo.repository.UniversityRepo;
+import com.example.demo.repository.UniversityRepository;
 import com.example.demo.service.UniversityService;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +12,7 @@ import java.util.List;
 @Service
 public class UniversityServiceImpl implements UniversityService {
 
-    private final UniversityRepo repository;
+    private final UniversityRepository repository;
 
     public UniversityServiceImpl(UniversityRepo repository) {
         this.repository = repository;
