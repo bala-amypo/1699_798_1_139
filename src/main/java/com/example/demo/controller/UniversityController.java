4bd494e90a000467ab345e0a3fd457cec47f4d72
bala-amypo <bala@amypo.in>
2025-12-22 13:21:31 +0000

    User commit

--- src/main/java/com/example/demo/controller/UniversityController.java
+++ src/main/java/com/example/demo/controller/UniversityController.java
@@ -1,7 +1,7 @@
 package com.example.demo.controller;
 
-import com.example.app.entity.University;
-import com.example.app.service.UniversityService;
+import com.example.demo.entity.University;
+import com.example.demo.service.UniversityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import io.swagger.v3.oas.annotations.tags.Tag;
