21f1f882e8cec0b4addac237e80f199c3621e616
bala-amypo <bala@amypo.in>
2025-12-22 05:08:36 +0000

    User commit

--- src/main/java/com/example/demo/entity/University.java
+++ src/main/java/com/example/demo/entity/University.java
@@ -19,7 +19,7 @@ public class University {
     @Column(nullable = false)
     private Boolean active = true;
 
-    // Getters and Setters
+   
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -24,7 +24,7 @@ public class User {
     @Column(nullable = false)
     private LocalDateTime createdAt = LocalDateTime.now();
 
-    // Getters and Setters
+   
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
