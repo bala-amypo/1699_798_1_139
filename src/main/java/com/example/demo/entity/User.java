009ca9fc6f0ed0ceb2de66f3b07ead838c2d9bd2
bala-amypo <bala@amypo.in>
2025-12-24 17:22:39 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,32 +1,21 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
-
+import lombok.Data;
 import java.util.Set;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Data // Generates getters, setters, toString, equals, and hashCode
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true, nullable = false)
-    private String email;
-
-    @Column(nullable = false)
+    private String username;
     private String password;
 
     @ElementCollection(fetch = FetchType.EAGER)
     private Set<String> roles;
-
-    public User(String email, String password, Set<String> roles) {
-        this.email = email;
-        this.password = password;
-        this.roles = roles;
-    }
 }
+
