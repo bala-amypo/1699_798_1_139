4901e505bdb1d895dcccd98953ae7f2f95fbdbc4
bala-amypo <bala@amypo.in>
2025-12-25 09:03:22 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UniversityServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UniversityServiceImpl.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class UniversityServiceImpl implements UniversityService {
 
     // injected by reflection in tests
+    @Autowired
     private UniversityRepository repository;
 
     @Override
